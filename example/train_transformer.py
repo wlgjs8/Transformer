e4dd37719e4245ccbcc0e0a0369c73b86909a45c
Namkwangwoon <nkw096@naver.com>
2022-04-28 16:10:30 +0900

    transformer training incomplete_1

--- example/train_transformer.py
+++ example/train_transformer.py
@@ -3,6 +3,7 @@ import sys
 import argparse
 import time
 from datetime import datetime
+from example.train_resnet import eval_training
 
 import numpy as np
 import torch
@@ -18,26 +19,116 @@ sys.path.append('.')
 sys.path.append('model')
 
 from config import settings
-from utils import get_network, CocoDataset, WarmUpLR, \
+from utils import get_network, CocoDataset, get_cifar100_train_dataloader, get_cifar100_test_dataloader, WarmUpLR, \
     most_recent_folder, most_recent_weights, last_epoch, best_acc_weights
     
 from model.optim import ScheduledOptim
 
+import model.constants as Constants
+from model.models import Transformer
+from model.optim import ScheduledOptim
 
 def train(epoch):
     
     start = time.time()
     net.train()
-    for i, (images, boxes, labels) in enumerate(train_loader):
-        print(i)
-        print(images)
-        print(boxes)
-        print(labels)
-        print()
+    # for i, (images, boxes, labels) in enumerate(train_loader):
+    #     print(i)
+    #     print(images)
+    #     print(boxes)
+    #     print(labels)
+    #     print()
 
-        images = images.cuda()
-        boxes = [b.cuda() for b in boxes]
-        labels = [l.cuda() for l in labels]
+    #     images = images.cuda()
+    #     boxes = [b.cuda() for b in boxes]
+    #     labels = [l.cuda() for l in labels]
+    for batch_index, (images, labels) in enumerate(cifar100_training_loader):
+    
+        if args.gpu:
+            labels = labels.cuda()
+            images = images.cuda()
+
+        optimizer.zero_grad()
+        outputs = net(images)
+        loss = loss_function(outputs, labels)
+        loss.backward()
+        optimizer.step_and_update_lr()
+
+        n_iter = (epoch - 1) * len(cifar100_training_loader) + batch_index + 1
+
+        # last_layer = list(net.children())[-1]
+        # for name, para in last_layer.named_parameters():
+        #     if 'weight' in name:
+        #         writer.add_scalar('LastLayerGradients/grad_norm2_weights', para.grad.norm(), n_iter)
+        #     if 'bias' in name:
+        #         writer.add_scalar('LastLayerGradients/grad_norm2_bias', para.grad.norm(), n_iter)
+
+        print('Training Epoch: {epoch} [{trained_samples}/{total_samples}]\tLoss: {:0.4f}\tLR: {:0.6f}'.format(
+            loss.item(),
+            optimizer._optimizer.param_groups[0]['lr'],
+            epoch=epoch,
+            trained_samples=batch_index * args.b + len(images),
+            total_samples=len(cifar100_training_loader.dataset)
+        ))
+
+        #update training loss for each iteration
+        writer.add_scalar('Train/loss', loss.item(), n_iter)
+
+        # if epoch <= args.warm:
+        #     optimizer.step()
+            
+    # for name, param in net.named_parameters():
+    #     layer, attr = os.path.splitext(name)
+    #     attr = attr[1:]
+    #     writer.add_histogram("{}/{}".format(layer, attr), param, epoch)
+        
+    finish = time.time()
+    
+    print('epoch {} training time consumed: {:.2f}s'.format(epoch, finish - start))
+    
+    
+@torch.no_grad()
+def eval_training(epoch=0, tb=True):
+
+    start = time.time()
+    net.eval()
+
+    test_loss = 0.0 # cost function error
+    correct = 0.0
+
+    for (images, labels) in cifar100_test_loader:
+
+        if args.gpu:
+            images = images.cuda()
+            labels = labels.cuda()
+
+        outputs = net(images)
+        loss = loss_function(outputs, labels)
+
+        test_loss += loss.item()
+        _, preds = outputs.max(1)
+        correct += preds.eq(labels).sum()
+
+    finish = time.time()
+    if args.gpu:
+        print('GPU INFO.....')
+        print(torch.cuda.memory_summary(), end='')
+    print('Evaluating Network.....')
+    print('Test set: Epoch: {}, Average loss: {:.4f}, Accuracy: {:.4f}, Time consumed:{:.2f}s'.format(
+        epoch,
+        test_loss / len(cifar100_test_loader.dataset),
+        correct.float() / len(cifar100_test_loader.dataset),
+        finish - start
+    ))
+    print()
+
+    #add informations to tensorboard
+    if tb:
+        writer.add_scalar('Test/Average loss', test_loss / len(cifar100_test_loader.dataset), epoch)
+        writer.add_scalar('Test/Accuracy', correct.float() / len(cifar100_test_loader.dataset), epoch)
+
+    return correct.float() / len(cifar100_test_loader.dataset)
+
 
 if __name__ == '__main__':
 
@@ -50,18 +141,77 @@ if __name__ == '__main__':
     parser.add_argument('-resume', action='store_true', default=False, help='resume training')
     args = parser.parse_args()
 
-    net = get_network(args)
-
-    #data preprocessing:
-    train_set = CocoDataset()
-
-    train_loader = DataLoader(train_set, batch_size=1, collate_fn=train_set.collate_fn, shuffle=False, num_workers=4, pin_memory=True)
-    # train_loader = DataLoader(train_set, batch_size=4, collate_fn=None, shuffle=False, num_workers=4, pin_memory=True)
+    net = Transformer(
+        #####
+    )
     
-    loss_function = nn.CrossEntropyLoss()       ## LabelSmoothin이 없을 시
+    #data preprocessing:
+    # train_set = CocoDataset()
+
+    # train_loader = DataLoader(train_set, batch_size=1, collate_fn=train_set.collate_fn, shuffle=False, num_workers=4, pin_memory=True)
+    # # train_loader = DataLoader(train_set, batch_size=4, collate_fn=None, shuffle=False, num_workers=4, pin_memory=True)
+    
+    cifar100_training_loader = get_cifar100_train_dataloader(
+        settings.CIFAR100_TRAIN_MEAN,
+        settings.CIFAR100_TRAIN_STD,
+        num_workers=4,
+        batch_size=args.b,
+        shuffle=True
+    )
+
+    cifar100_test_loader = get_cifar100_test_dataloader(
+        settings.CIFAR100_TRAIN_MEAN,
+        settings.CIFAR100_TRAIN_STD,
+        num_workers=4,
+        batch_size=args.b,
+        shuffle=True
+    )
+    
+    
+    loss_function = nn.CrossEntropyLoss()       ## LabelSmoothing이 없을 시
     optimizer = ScheduledOptim(
         optim.Adam(net.parameters(), betas=(0.9, 0.98), eps=1e-09),
-        opt.lr_mul, opt.d_model, opt.n_warmup_steps)
-    iter_per_epoch = len(train_loader)
+        args.lr, args.d_model, args.warm)       ## args.d_model은 2048 고정?? 인자로??
+    iter_per_epoch = len(cifar100_training_loader)
+    
+    # if args.resume:
+    checkpoint_path = os.path.join(settings.CHECKPOINT_PATH, args.net, settings.TIME_NOW)
+    
+    ## log 작성??
+    writer = SummaryWriter(log_dir=os.path.join(
+        settings.LOG_DIR, args.net, settings.TIME_NOW
+    ))
+    input_tensor = torch.Tensor(1, 3, 32, 32)           ## input tensor 사이즈 변경 필요
+    if args.gpu:
+        input_tensor = input_tensor.cuda()
+    writer.add_graph(net, input_tensor)
+    ##
+    
+    if not os.path.exists(checkpoint_path):
+        os.makedirs(checkpoint_path)
+    checkpoint_path = os.path.join(checkpoint_path, '{net}-{epoch}-{type}.pth')
+    
+    best_acc = 0.0
+    
+    for epoch in range(1, settings.EPOCH + 1):
+        # if epoch > args.warm:
+        #     optimizer.step(epoch)
+            
+        train(epoch)
+        acc = eval_training(epoch)
+        
+        if epoch > settings.MILESTONES[1] and best_acc < acc:
+            weights_path = checkpoint_path.format(net='transformer', epoch=epoch, type='best')
+            print('saving weights file to {}'.format(weights_path))
+            torch.save(net.state_dict(), weights_path)
+            best_acc = acc
+            continue
+        
+        if not epoch % settings.SAVE_EPOCH:
+            weights_path = checkpoint_path.format(net='transformer', epoch=epoch, type='regular')
+            print('saving weights file to {}'.format(weights_path))
+            torch.save(net.state_dict(), weights_path)
+            
+    writer.close()
 
     # return 0 
